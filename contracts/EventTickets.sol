46da1a3fc8e3355ce91403964ff9fac91459e785
Y'z Portfolio(Yasuko Tamura) <info@yz-portfolio.info>
2020-06-27 13:00:45 +0900

    Finished
    Some functions and modifiers are not clear. But I could compile it.

--- contracts/EventTickets.sol
+++ contracts/EventTickets.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.5.0;
      */
 
 contract EventTickets {
-  address public owner  = msg.sender(address,payable);
+  address public owner  = msg.sender();
   constructor() public {
   owner = msg.sender;
   }
@@ -17,7 +17,7 @@ contract EventTickets {
      */
 
     mapping (address => bool) public enrolled;
-    address public owner = msg.sender(address,payable);
+    address public owner = msg.sender();
     address payable[] public subscribers;
     uint   TICKET_PRICE = 100 wei;
 
@@ -28,19 +28,18 @@ contract EventTickets {
         The "buyers" field should keep track of addresses and how many tickets each buyer purchases.
     */
 
-var EventTickets = artifacts.require('EventTickets')
-let catchRevert =require(".exceptionHelpers.js").catchRevert
+const eventTickets = artifacts.require('EventTickets');
+await catchRevert =require(".exceptionHelpers.js").catchRevert;
 
-contract('EventTicket', function(accounts){
-  async() => {
-    const instance = await EventTickets.new(descriotion, website (URL), totalTickets, sales, buyers,isOpen)
-    await catchRevert(instance.buyTickets(1, {from: secondAccount, value: web3.utils.toWei)"100", "ether")}))
-  })
-}
+const eventTickets;
+ function(accounts){
+  async();
+    const instance = await (descriotion, website (URL), totalTickets, sales, buyers,isOpen);
+    await catchRevert;(instance.buyTickets(1, (web3.utils.toWei),'100', 'ether'),);
     Event myEvent;
-    Event LogBuyTickets(myEvent);
-    Event LogGetRefund(myEvent);
-    Event LogEndSale(myEvent);
+    Event LogBuyTickets;
+    Event LogGetRefund;
+    Event LogEndSale;
 
     /*
         Define 3 logging events.
@@ -48,15 +47,20 @@ contract('EventTicket', function(accounts){
         LogGetRefund should provide information about the refund requester and the number of tickets refunded.
         LogEndSale should provide infromation about the contract owner and the balance transferred to them.
     */
-    modifier verifyCaller(address _address){ require (msg.sender == _address); _;}
 
-      modifier paidEnough(uint _price){ require(msg.value >= _price); _;}
-      modifier checkValue(uint _amount){ require(msg.value >= _amount); _;}
+
+      function(address _address) require;
+       (msg.sender == _address); _;
+
+      function(uint _price) require;
+      (msg.value >= _price); _;
+      function(uint _amount) require;
+      (msg.value >= _amount); _;
     /*
         Create a modifier that throws an error if the msg.sender is not the owner.
     */
-function owner(address receiver, uint _amount) public {
-  require(msg.sender == owner, "Must be owner.");
+function(address receiver, uint _amount) require;
+(msg.sender == owner, "Must be owner.");
   balances[receiver] += amount;
 
   }
@@ -67,28 +71,27 @@ function owner(address receiver, uint _amount) public {
         Set the appropriate myEvent details.
     */
   constructor() public {
-    address public owner = msg.sender(address,payable);
-        event myEvent(description,website (URL), totalTickets, sales)
+    address owner = msg.sender(address);
+        Event myEvent;(description,website (URL), totalTickets, sales);
     /*
         Define a function called readEvent() that returns the event details.
         This function does not modify state, add the appropriate keyword.
         The returned details should be called description, website, uint totalTickets, uint sales, bool isOpen in that order.
     */
-    function readEvent()
-        public
-        returns myEvent(string memory description, string memory website, uint totalTickets, uint sales, bool)
-    {
+     readEvent(address);
+        address owner = msg.sender(address);
+        Event readEvent;
+         (string memory description, string memory website, uint totalTickets, uint sales, bool isOpen,)=
 
-    }
 
     /*
         Define a function called getBuyerTicketCount().
         This function takes 1 argument, an address and
         returns the number of tickets that address has purchased.
     */
-    function getBuyerTicketCount()
-    public
-    returns myEvent(address,totalTicket)
+    getBuyerTicketCount(address);
+    Event myEvent;
+    (address,totalTicket);
 
 
     /*
@@ -106,14 +109,15 @@ function owner(address receiver, uint _amount) public {
             - refund any surplus value sent with the transaction
             - emit the appropriate event
     */
-    function buyerTickets(uint totalTicket) public returns (uint) {
-      event isOpen(totalTicket)
-      uint totalTicket;
-    function set(uint z) public {
+    buyerTickets(uint, totalTicket);
+      Event isOpen;
+      (uint,totalTicket);
+
+    set(uint, z);
         totalTicket = z;
         return totalTicket;
 
-    }
+
 
     /*
         Define a function called getRefund().
@@ -124,14 +128,14 @@ function owner(address receiver, uint _amount) public {
             - Transfer the appropriate amount to the refund requester.
             - Emit the appropriate event.
     */
-    function getRefund(uint amount,totalTicket) public returns{
+    getRefund(uint, amount,totalTicket);
+    {
           require(balance[msg.sender] >= amount, "Insufficient funds");
           balances[msg.sender] -= amount;
           balances[receiver] += amount;
           emit getRefund(msg.sender, receiver ,amount);
         }
 
-        }
 
     /*
         Define a function called endSale().
@@ -142,7 +146,8 @@ function owner(address receiver, uint _amount) public {
             - transfer the contract balance to the owner
             - emit the appropriate event
     */
-    function endSale(address receiver, uint amount) public returns{
+    endSale (address, uint, amount);
+    {
           require(balance[msg.sender] >= amount, "Insufficient funds");
           balances[msg.sender] -= amount;
           balances[receiver] += amount;
